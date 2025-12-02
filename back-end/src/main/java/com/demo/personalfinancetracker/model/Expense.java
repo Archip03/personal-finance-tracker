d7250b1c194ecf5c6702c2cb5dd1a27f24037168
Archip03 <patelarchi798@gmail.com>
2025-12-02 14:00:21 +0530

    Initial commit - Personal Finance Tracker

---
+++ back-end/src/main/java/com/demo/personalfinancetracker/model/Expense.java
@@ -0,0 +1,64 @@
+package com.demo.personalfinancetracker.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.time.LocalDate;
+
+@Entity
+public class Expense {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String description;
+    private Double amount;
+    private LocalDate date;
+
+    // ---- Constructors ----
+    public Expense() {
+    }
+
+    public Expense(Long id, String description, Double amount, LocalDate date) {
+        this.id = id;
+        this.description = description;
+        this.amount = amount;
+        this.date = date;
+    }
+
+    // ---- Getters & Setters ----
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+}
